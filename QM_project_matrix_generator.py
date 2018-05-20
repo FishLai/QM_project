f3bdc2c313334910759fee7383f7d424cf24a994
FishLai <38682183+FishLai@users.noreply.github.com>
2018-05-20 16:53:46 +0800

    Update QM_project_matrix_generator.py
    code generate 10*10 matrix, the parameters are use 1

--- QM_project_matrix_generator.py
+++ QM_project_matrix_generator.py
@@ -6,13 +6,30 @@ Created on Wed May 16 21:54:14 2018
 """
 
 import numpy as np;
-""" create N*N zeros matrix first """
-N = 10;
-row = [0]*N;
+from math import log;
+"""
+    create N*N zeros matrix first
+"""
+N = 10; #set up size of matrix
+row = [0.00000]*N; 
 for j in range(N):
-    row[j] = [0]*N;
-""" defined our hamition matrix """
-for j in range(10):
-    for i in range(10):
-        row[j][i] = j*i;
-print(row);
+    row[j] = [0.00000]*N; #create each element
+print(row) #check zero matrix is generated
+"""
+    defined our hamition matrix
+"""
+l = 2; #use l=2 state construct matrix
+a = 1; #the interval on variable axies
+Omega = 1; #temperature omega value
+rho0 = 1; #temperature rho0 value
+for j in range(N):
+    for i in range(j, j+3):
+        #print(j);
+        if i == j :
+            row[j][i] = 1/(a**2) - 1/((j+1)*(a**2));
+        elif i == (j+1) and i < N :
+            row[j][i] = (-2/a**2) + 1/((j+1)*a**2) - Omega*log(a/rho0, 10) - (l**2)/(((j+1)*a)**2);
+        elif i == (j+2) and i < N :
+            row[j][i] = 1/(a**2);
+                
+print(row); #look the matrix
