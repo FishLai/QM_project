f7958e0b0716cd18df158902064c7ade5143f79a
FishLai <38682183+FishLai@users.noreply.github.com>
2018-06-20 18:00:04 +0800

    Update QM_project_matrix_generator.py
    I separate coefficient for each derivation

--- QM_project_matrix_generator.py
+++ QM_project_matrix_generator.py
@@ -20,17 +20,24 @@ def Operator_generator(upLimit, quantumNum, interval):
     """
     l = quantumNum; #use l=2 state construct matrix
     a = interval; #the interval on variable axies
-    Omega = 1.; #temperature omega value
-    rho0 = 1.; #temperature rho0 value
+    A21 = 1./a**2;
+    A22 = -2./a**2;
+    A23 = 1/a**2;
     for j in range(N):
         for i in range(j - 1, j + 2):
-            #print(j);
+            n = j+1;
+            A11 = -1./(n*(a**2));
+            A12 = 1./(n*(a**2));
+            A13 = 0;
+            A01 = 0;
+            A02 = -log(n*a)-(l**2)/((n*a)**2);
+            A03 = 0;
             if i == (j -1) and i >= 0 :
-                Matrix[j][i] = 1./(a**2) - 1./((j+1)*(a**2));
+                Matrix[j][i] = A21 + A11 + A01;#1./(a**2) - 1./((j+1)*(a**2));
             elif i == j:
-                Matrix[j][i] = (-2./a**2) + 1./((j+1)*a**2) - Omega*log(a/rho0, 10) - (l**2)/(((j+1)*a)**2);
+                Matrix[j][i] = A22 +A12 +A02;#(-2./a**2) + 1./((j+1)*a**2) - log((j+1)*a, 10) - (l**2)/(((j+1)*a)**2);
             elif i == (j + 1) and i < N :
-                Matrix[j][i] = 1./(a**2);
+                Matrix[j][i] = A23 +A13 +A03;#1./(a**2);
     print(Matrix); #look the matrix
     return Matrix;
 # Operator_generator(5, 0, 0.5); #test result
