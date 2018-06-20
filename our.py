c893905870cf4a16ea0432c61ebb6e985a0d02c5
FishLai <38682183+FishLai@users.noreply.github.com>
2018-06-20 17:06:25 +0800

    Add files via upload

---
+++ our.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+generate by FishLai 
+"""
+
+from temp import generator as OG;
+import numpy as np;
+from numpy import linalg as LA;
+import matplotlib.pyplot as plt;
+
+N = 3000;
+Matrix = OG(N, 0, 0.004); #OG(upLimit, quantumNum, interval)
+
+E_value, E_vector = LA.eigh(Matrix);
+#print(Matrix);
+#print(E_value);
+#print(E_vector);
+
+x = np.array([0]*N, dtype=np.float32);
+for i in range (N):
+    x[i] = (i+1)*0.004;
+
+plt.plot(x, E_vector[0]);
+plt.show();
+
