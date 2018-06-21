3b1796f5c9513e96108558d4bfed9a09ed660283
FishLai <38682183+FishLai@users.noreply.github.com>
2018-06-21 17:45:41 +0800

    Update E_value-vector.py

--- E_value-vector.py
+++ E_value-vector.py
@@ -12,14 +12,19 @@ import numpy as np;
 
 N = 1000;
 a = 0.001;
-Matrix = OG(N, 0, a); 
+Matrix = OG(N, 0, a); #three parameter about number of points, quantum number and intervial
 Evalue, Evector = LA.eigh(Matrix, UPLO='L');
 #print(Evalue);
 #print(Evector);
+"""
+create x axies;
+"""
 x = np.array([0]*N, dtype=float);
 for i in range(N):
-    x[i] = (i+1)*a;
-
+    x[i] = (i+1)*a;    
 #print(Evector[0]);
+"""
+plot wave function;
+"""
 plt.plot(x, Evector[0]);
 plt.show();
